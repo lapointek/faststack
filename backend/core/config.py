7791073a3cf64c878bb0d9744fed39db1ca1fb9f
lapointek <lapointek.dev@gmail.com>
2025-12-15 18:28:57 -0500

    feat: create Settings class to load variables from .env

---
+++ backend/core/config.py
@@ -0,0 +1,31 @@
+from typing import List
+
+# Advance python type handling
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+
+# Inherit base settings from pydantic
+class Settings(BaseSettings):
+    # Load variables from .env
+    API_PREFIX: str = "/api"
+    DEBUG: bool = False
+    DATABASE_URL: str
+    ALLOWED_ORIGINS: str = ""
+    OPENAPI_API_KEY: str
+
+    # Convert ALLOWED_ORIGINS into a list
+    @field_validator("ALLOWED_ORIGINS")
+    def parse_allowed_origins(cls, v: str) -> List[str]:
+        # Return value separated by commas if exist otherwise return empty list
+        return v.split(",") if v else []
+
+    # Set Configuration
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+
+
+# Instantiate class
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+# Import settings variables
+from core.config import settings
+
+app = FastAPI(
+    # Documentation
+    title="Choose Your Own Adventure Game API",
+    description="api to generate cool stories",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    # Have API used from a different origin
+    # Cross Origin Resource Sharing
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    main()
+    # Run webserver
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
