23caeca441b71116a155493cb9a1adfad8466516
lapointek <lapointek.dev@gmail.com>
2025-12-16 18:54:04 -0500

    feat: setup SQLAlchemy

---
+++ backend/db/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from core.config import settings
+
+# create connection to database
+engine = create_engine(settings.DATABASE_URL)
+
+# generate new database session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# tables inherit from this
+Base = declarative_base()
+
+
+# insures we do not have more than one session
+def get_db():
+    # instantiate new session
+    db = SessionLocal()
+    try:
+        # suspend function and retrieve values one at a time
+        yield db
+    finally:
+        db.close()
+
+
+# create corresponding tables in the database
+def create_tables():
+    Base.metadata.create_all(bind=engine)
---
+++ backend/models/story.py
@@ -0,0 +1,4 @@
+# SQLalchemy - ORM Object Relational Mapping
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
