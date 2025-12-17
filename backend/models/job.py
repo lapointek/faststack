a7b6d0256d84d2fab8542ef9aa328b937395152c
lapointek <lapointek.dev@gmail.com>
2025-12-17 13:48:07 -0500

    feat(models): add StoryJob class with columns in SQLAlchemy

---
+++ backend/models/job.py
@@ -0,0 +1,20 @@
+# job is an intent to make a story
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+
+class StoryJob(Base):
+    __tablename__ = "story_jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    # id that uses a long string value
+    job_id = Column(String, index=True, unique=True)
+    # id of the user who created this job
+    session_id = Column(String, index=True)
+    theme = Column(String)
+    status = Column(String)
+    story_id = Column(Integer, nullable=True)
+    error = Column(String, nullable=True)
+    # timestamp when job was created
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_job = Column(DateTime(timezone=True), nullable=True)
