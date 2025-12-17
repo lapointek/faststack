02c46aaf9f39cc7cc0309d5e2739d5fedb83c2ad
lapointek <lapointek.dev@gmail.com>
2025-12-17 16:32:11 -0500

    fix(models): add import for Base in job

--- backend/models/job.py
+++ backend/models/job.py
@@ -2,6 +2,8 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 
+from db.database import Base
+
 
 class StoryJob(Base):
     __tablename__ = "story_jobs"
