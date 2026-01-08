40ac01b48569f66f99a27b0ae351dc75aea8b388
lapointek <lapointek.dev@gmail.com>
2026-01-08 15:56:27 -0500

    chore: add docker-compose file and Dockerfile to frontend chore: update target property in vite config file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StoryLoader from "./components/StoryLoader";
 
 function App() {
   return (
-    <></>
-  )
+    <Router>
+      <header>
+        <h1>Interactive Story Generator</h1>
+      </header>
+      <main>
+        <Routes>
+          <Route path={"/story/:id"} element={<StoryLoader />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
