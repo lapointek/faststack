3b4d0478185a4df1e0896221a06bd0b4fc00b74c
lapointek <lapointek.dev@gmail.com>
2026-01-11 18:36:19 -0500

    chore(backend): remove run command in main.py

--- backend/main.py
+++ backend/main.py
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     # Run webserver
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
